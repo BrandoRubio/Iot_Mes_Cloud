20554fde8da9c581179c1809fff86bd03e52acec
BrandoRubio <33293044+BrandoRubio@users.noreply.github.com>
2025-05-12 12:31:01 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IoT_Mes_Cloud',
+  webDir: 'www'
+};
+
+export default config;
